55f8f01307ba0991c3e8ae95582f0e54a9037a0d
Christian Todd <christiantodd27@icloud.com>
2017-04-19 22:56:28 -0400

    more robust check for errors of interest.

--- middleware/error-handling.js
+++ middleware/error-handling.js
@@ -5,18 +5,18 @@ const {sendEmail} = require('../emailer');
 const {ALERT_FROM_EMAIL, ALERT_TO_EMAIL} = process.env;
 
 const errorHandlingMiddleware = () => (err, req, res, next) => {
-  if (err.name === 'BizzError'){
-    // Do nothing with a BizzError, just print to the console
-    console.log(`Just a ${err.name}, nothing to see here.`);
-  } else {
-    // FooError and BarErrors get their error data send as an email
+  if (err instanceof FooError || err instanceof BarError){
+    // check for errors that should send an email alert
     let emailData = {
       from: ALERT_FROM_EMAIL,
       to: ALERT_TO_EMAIL,
       subject: `ALERT: a ${err.name} occurred.`,
       text: `MESSAGE: ${err.message}. \n STACK-TRACE: ${err.stack}.`
-    }
+    };
     sendEmail(emailData);
+  } else {
+    // log that some other error occurred
+    console.log(`Just a ${err.name}, nothing to see here.`);
   }
 }
 
